d68ba1c734d36c8ec276f97a3e69c1bc887487de
maxkl <max@maxkl.de>
2020-08-01 13:27:48 +0200

    Start multiple workers

--- static/main.js
+++ static/main.js
@@ -1,20 +1,43 @@
 
-const w = new Worker('worker.js');
-w.addEventListener('message', function (evt) {
-    const message = evt.data;
-    switch (message.name) {
-        case 'loaded':
-            console.log('Loaded');
-            break;
-        case 'load error':
-            console.error('Load failed:', message.error);
-            break;
-        default:
-            console.error('Invalid message:', message);
+function range(start, stop, step = 1) {
+    if (typeof stop === 'undefined') {
+        stop = start;
+        start = 0;
     }
-});
-w.postMessage({ name: 'load' });
+    return Array(Math.ceil((stop - start) / step))
+        .fill(start)
+        .map((x, y) => x + y * step);
+}
 
-window.say_hello = function (who) {
-    w.postMessage({ name: 'hello', who: who });
-};
+function startWorker() {
+    return new Promise(((resolve, reject) => {
+        const worker = new Worker('worker.js');
+
+        worker.addEventListener('message', function (evt) {
+            const message = evt.data;
+            switch (message.name) {
+                case 'loaded':
+                    resolve(worker);
+                    break;
+                case 'load error':
+                    reject(message.error);
+                    break;
+                default:
+                    console.error('Invalid message:', message);
+            }
+        });
+
+        worker.postMessage({ name: 'load' });
+    }));
+}
+
+(async function () {
+    const cpuCount = window.navigator.hardwareConcurrency || 1;
+    const workers = await Promise.all(range(cpuCount).map(i => startWorker()));
+
+    console.info('Started ' + workers.length + ' workers');
+
+    window.sayHello = function (who, index) {
+        workers[index].postMessage({ name: 'hello', who: who });
+    };
+})().catch(err => console.error(err));
--- static/worker.js
+++ static/worker.js
@@ -7,7 +7,7 @@ onmessage = function (evt) {
         case 'load':
             wasm_bindgen('./pkg/raytracer_web_bg.wasm')
                 .then(wasm => postMessage({ name: 'loaded' }))
-                .catch(err => postMessage({ name: 'load error', error: err }));
+                .catch(err => postMessage({ name: 'load error', error: err.toString() }));
             break;
         case 'hello':
             wasm_bindgen.say_hello(message.who);
